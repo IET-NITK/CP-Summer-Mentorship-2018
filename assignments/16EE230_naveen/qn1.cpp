e1edb9a620d8f2a86f39481e51964cf4d7311270
naveen <mnaveenkumar2009@gmail.com>
2018-05-12 10:07:51 +0530

    added temporary assignment 1

---
+++ assignments/16EE230_naveen/qn1.cpp
@@ -0,0 +1,5 @@
+// https://www.codechef.com/COOK85/problems/GAMSTICK/
+// solution link : https://www.codechef.com/viewsolution/15057568
+
+
+// code :
---
+++ assignments/16EE230_naveen/qn2.cpp
@@ -0,0 +1,5 @@
+// https://www.codechef.com/LTIME52/problems/C00K0FF/
+// solution link : https://www.codechef.com/viewsolution/15559981
+
+
+// code :
