5975fb2a5f8c094aef4eca1ca525d57f3aa7695a
saurabh8522 <saurabhsinghal.171co240@nitk.edu.in>
2018-05-12 13:09:00 +0530

    added question 2 of assignment 1

---
+++ assignments/17CO240_saurabh/assignment1/q2.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<string>
+using namespace std;
+int main(){
+int t,n;
+string s;
+vector <int> a={0,0,0,0,0};
+cin>>t;
+for(int i=0;i<t;i++)
+{cin>>n;
+vector <int>::iterator it=a.begin();
+for(int j=0;j<n;j++)
+{cin>>s;
+if(s=="cakewalk")
+(*it)++;
+else if(s=="simple")
+(*(it+1))++;
+else if(s=="easy")
+(*(it+2))++;
+else if(s=="easy-medium"||s=="medium")
+(*(it+3))++;
+else if(s=="medium-hard"||s=="hard")
+(*(it+4))++;}
+it=a.begin();
+if(((*it)>=1)&&((*(it+1))>=1)&&((*(it+2))>=1)&&((*(it+3))>=1)&&((*(it+4))>=1))
+cout<<"Yes"<<endl;
+else
+cout<<"No"<<endl;
+for(it=a.begin();it!=a.end();it++)
+*it=0;}}
+
+
+
